a3dbd5de91a5afed24a9efdd9ae7731a09ae63b4
ThupakulaGovardhan <govardhan3443@gmail.com>
2023-08-21 18:07:06 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:test/calculatepage.dart';
+
 
 
 
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-    home: CalculatorPage()
+    home: 
     );
   }
 }
