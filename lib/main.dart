bee67021328938374f37f075e2711aab5c899296
pravalika545 <102211643+pravalika545@users.noreply.github.com>
2023-08-22 16:06:56 +0530

    weather_code

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-import 'screens/instagram_screen.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -16,12 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Tasks',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: InstagramScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Tasks',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyApp());
   }
 }
