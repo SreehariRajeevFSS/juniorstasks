28e85d0ec620b0135584fd31754f72086052b1d7
ThupakulaGovardhan <govardhan3443@gmail.com>
2023-08-22 16:47:48 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:test/weatherscreen.dart';
+
 
 
 
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-    home: WeatherScreen()
+    home: 
     );
   }
 }
