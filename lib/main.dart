9dac80781e4d5609c477591873a02ba4815d078c
ThupakulaGovardhan <govardhan3443@gmail.com>
2023-08-23 18:17:36 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:test/screens/instagram_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-    home: InstagramPage()
+    home:
     );
   }
 }
