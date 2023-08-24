690db216a0fc84a3dc69f51d121d3085c3885b15
pravalika545 <102211643+pravalika545@users.noreply.github.com>
2023-08-24 13:50:37 +0530

    code

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'package:get/get.dart';
-import 'package:test/screens/gallery_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,6 +18,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: GalleryScreen());
+        home: MyApp());
   }
 }
