6c40f59d610ae29bf1171e775684915095eee672
pravalika545 <102211643+pravalika545@users.noreply.github.com>
2023-08-25 12:12:11 +0530

    code_changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,11 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Taks',
+        title: 'Tasks',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
